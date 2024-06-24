7a27db3bfa8b606a29a8a0b9abe040b240c73639
Cliff <Ockiyacliffc@gmail.com>
2024-06-24 01:04:16 +0100

    3d model init

--- App.js
+++ App.js
@@ -1,17 +1,18 @@
 import { Canvas } from "@react-three/fiber/native";
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Suspense } from "react";
+import { StyleSheet, View } from "react-native";
 import { BottomUI } from "./components/BottomUI";
+import { Sandwich } from "./components/Sandwich";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Canvas camera={{ position: [-2, 2.5, 5], fov: 30 }}>
         <color attach="background" args={["#512DA8"]} />
-        <mesh>
-          <boxGeometry args={[1, 1, 1]}/>
-          <meshBasicMaterial color="hotpink" />
-        </mesh>
+        <Suspense>
+          <Sandwich />
+        </Suspense>
       </Canvas>
       <BottomUI />
       <StatusBar style="auto" />
@@ -22,8 +23,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    // alignItems: 'center',
-    // justifyContent: 'center',
+    backgroundColor: "#fff",
   },
 });
